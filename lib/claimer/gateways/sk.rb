05b8fbe9845df496c04f88120c0e1fa3b73b1396
Brady Bouchard <brady@thewellinspired.com>
2014-08-13 23:11:39 -0600

    Added SK test submission file.

--- lib/claimer/gateways/sk.rb
+++ lib/claimer/gateways/sk.rb
@@ -1,10 +1,11 @@
 module Claimer
   class SK < Gateway
 
-    attr_accessor :records
+    attr_accessor :records, :first_claim_number
 
     def initialize(values = {})
       self.records = []
+      self.first_claim_number = 10000
     end
 
     def add_record
@@ -12,32 +13,53 @@ module Claimer
 
     private
 
-    def header_record(record)
+    def header_record(selected_records)
       [
         '10', # Record type number
-        record.doctor_number.to_s.rjust(4, '0'),
+        selected_records.first.doctor_number.to_s.rjust(4, '0'),
         '000000', # Filler
-        record.clinic_number.to_s.rjust(3, '0'),
-        record.clinic_name.to_s[0..24].rjust(25, '0'),
-        record.clinic_address.to_s[0..24].rjust(25, '0'),
-        record.clinic_city_and_prov.to_s[0..24].rjust(25, '0'),
-        record.clinic_postal_code.to_s[0..24].rjust(25, '0'),
+        selected_records.first.clinic_number.to_s.rjust(3, '0'),
+        selected_records.first.clinic_name.to_s[0..24].rjust(25, '0'),
+        selected_records.first.clinic_address.to_s[0..24].rjust(25, '0'),
+        selected_records.first.clinic_city_and_prov.to_s[0..24].rjust(25, '0'),
+        selected_records.first.clinic_postal_code.to_s[0..24].rjust(25, '0'),
         '8', # Submission type: By Internet.
         '' # Practitioner billing.
       ].join('')
     end
 
-    def record_format(record, claim_number)
+    def trailer_record(selected_records)
       [
-        '50', # Record type number
+        '90', # Record type number
+        selected_records.first.doctor_number.to_s.rjust(4, '0'),
+        '999999', # Filler
+        selected_records.count.to_s.rjust(5, '0'),
+        selected_records.reject{ |r| r.comments.nil? || r.comments.length == 0 }.count.to_s.rjust(5, '0'),
+        selected_records.count.to_s.rjust(7, '0'),
+        '69' # Filler
+      ].join('')
+    end
+
+    def record_format(record)
+      if record.in_hospital
+        hospital_record_format(record)
+      else
+        visit_record_format(record)
+      end
+    end
+
+    def hospital_record_format(record)
+      [
+        '57', # Record type number
         record.doctor_number.to_s.rjust(4, '0'),
-        claim_number.to_s[0..4],
+        record.claim_number.to_s[0..4],
         '0', # Placeholder for claim sequence number.
         record.hsn.to_s[0..8],
         record.dob.strftime('%m%y'),
+        record.sex.to_s.upcase,
         "#{record.last_name} #{record.first_name}"[0..24].ljust(25),
         record.icd9[0..2],
-        ''.rjust(4), # Referring doctor number.
+        record.referring_doctor_number.rjust(4),
         record.date_of_service.strftime('%d%m%y'),
         record.number_of_units.to_s.rjust(2, '0'),
         '1', # Location of service
@@ -49,27 +71,78 @@ module Claimer
       ].join('')
     end
 
-    def comment_format(record, claim_number)
+    def visit_record_format(record)
+      [
+        '50', # Record type number
+        record.doctor_number.to_s.rjust(4, '0'),
+        record.claim_number.to_s[0..4],
+        '0', # Placeholder for claim sequence number.
+        record.hsn.to_s[0..8],
+        record.dob.strftime('%m%y'),
+        record.sex.to_s.upcase,
+        "#{record.last_name} #{record.first_name}"[0..24].ljust(25),
+        record.icd9[0..2],
+        record.referring_doctor_number.rjust(4),
+        record.date_of_service.strftime('%d%m%y'),
+        record.number_of_units.to_s.rjust(2, '0'),
+        '1', # Location of service
+        record.fee_code.to_s.rjust(4, '0'),
+        record.fee.to_s.gsub('.','').gsub('$','').rjust(6, '0'),
+        '1', # Physician code.
+        '8', # Submission by internet.
+        ' ' * 19 # Filler.
+      ].join('')
+    end
+
+    def comment_format(record)
       return nil if record.comments.nil? || record.comments.length == 0
       [
         '60', # Record type number
         record.doctor_number.to_s.rjust(4, '0'),
-        claim_number.to_s[0..4],
+        record.claim_number.to_s[0..4],
         '0', # Placeholder for claim sequence number.
         record.hsn.to_s[0..8],
         record.comments.to_s[0..76] # Comments.
       ].join('')
     end
 
-    def collate
+    # Claim numbers are assigned sequentially starting at first_claim_number.
+    # Claim sequence numbers are all 0 - this part of the API (if you can call it an API)
+    # hasn't been implemented yet.
+    def assign_claim_numbers
+      c = self.first_claim_number
+      self.records.each_with_index do |r|
+        r.claim_number ||= c + r - 1
+        r.claim_sequence_number ||= 0
+      end
     end
 
-    def publish
-      self.records.each_with_index.map do |r, i|
-        formatted_result = record_format(r, i)
-        formatted_comment = comment_format(r, i)
-        [ formatted_result, formatted_comment ].compact.join("\n")
-      end.join("\n")
+    # MSB requires records to be sent in clinic-doctor sets.
+    def collate_records
+
+      assign_claim_numbers
+
+      doctor_numbers = self.records.map{ |r| r.doctor_number }.uniq
+      clinic_numbers = self.records.map{ |r| r.clinic_number }.uniq
+
+      clinic_numbers.map do |cn|
+        doctor_numbers.map do |dn|
+          selected_records = self.records.select{ |r| r.doctor_number == dn && r.clinic_number == cn }
+          [
+            header_record(selected_records),
+            selected_records.map do |r|
+              formatted_result = record_format(r)
+              formatted_comment = comment_format(r)
+              [ formatted_result, formatted_comment ].compact.join("\n")
+            end.join("\n"),
+            trailer_record(selected_records)
+          ].join("\n")
+        end
+      end
+    end
+
+    def finalize_records!
+
     end
 
   end
--- lib/claimer/record.rb
+++ lib/claimer/record.rb
@@ -1,29 +1,33 @@
 module Claimer
   class Record
 
-    attr_accessor :doctor_number, :clinic_number, :clinic_address,
-      :clinic_city_and_prov, :clinic_postal_code, :hsn, :dob, :sex,
-      :first_name, :last_name, :icd9, :date_of_service, :number_of_units,
-      :fee, :fee_code, :claim_number
+    attr_accessor :doctor_number, :referring_doctor_number, :clinic_number,
+      :clinic_address, :clinic_city_and_prov, :clinic_postal_code, :hsn, :dob,
+      :sex, :first_name, :last_name, :icd9, :date_of_service, :end_date_of_service,
+      :number_of_units, :fee, :fee_code, :claim_number, :claim_sequence_number, :in_hospital
 
     def initialize(values = {})
-      self.doctor_number        = values[:doctor_number]
-      self.clinic_number        = values[:clinic_number]
-      self.clinic_address       = values[:clinic_address]
-      self.clinic_city_and_prov = values[:clinic_city_and_prov]
-      self.clinic_postal_code   = values[:clinic_postal_code]
-      self.hsn                  = values[:hsn]
-      self.dob                  = values[:dob]
-      self.sex                  = values[:sex]
-      self.first_name           = values[:first_name]
-      self.last_name            = values[:last_name]
-      self.icd9                 = values[:icd9]
-      self.date_of_service      = values[:date_of_service]
-      self.number_of_units      = values[:number_of_units]
-      self.fee                  = values[:fee]
-      self.fee_code             = values[:fee_code]
-      self.comments             = values[:comments]
-      self.claim_number         = values[:claim_number] || 0
+      self.doctor_number           = values[:doctor_number]
+      self.referring_doctor_number = values[:referring_doctor_number] || ''
+      self.clinic_number           = values[:clinic_number]
+      self.clinic_address          = values[:clinic_address]
+      self.clinic_city_and_prov    = values[:clinic_city_and_prov]
+      self.clinic_postal_code      = values[:clinic_postal_code]
+      self.hsn                     = values[:hsn]
+      self.dob                     = values[:dob]
+      self.sex                     = values[:sex]
+      self.first_name              = values[:first_name]
+      self.last_name               = values[:last_name]
+      self.icd9                    = values[:icd9]
+      self.date_of_service         = values[:date_of_service]
+      self.end_date_of_service     = values[:end_date_of_service]
+      self.number_of_units         = values[:number_of_units]
+      self.fee                     = values[:fee]
+      self.fee_code                = values[:fee_code]
+      self.comments                = values[:comments]
+      self.claim_number            = values[:claim_number] || 0
+      self.claim_sequence_number   = values[:claim_sequence_number] || 0
+      self.in_hospital             = values[:in_hospital] || false
     end
 
   end
